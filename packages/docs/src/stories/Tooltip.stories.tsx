dd064b3c91b0f4b293d3a6b19e62b560321e1e63
Pedro-Augusto-Barbosa-Aparecido <77130441+Pedro-Augusto-Barbosa-Aparecido@users.noreply.github.com>
2023-01-04 17:41:12 -0300

    feat tooltip component

---
+++ packages/docs/src/stories/Tooltip.stories.tsx
@@ -0,0 +1,40 @@
+import type { StoryObj, Meta } from "@storybook/react";
+
+import {
+  Box,
+  Text,
+  Tooltip,
+  TooltipProps,
+  TooltipProvider,
+} from "@taurus-ui/react";
+
+export default {
+  title: "Surfaces/Tooltip",
+  component: Tooltip,
+  args: {
+    message: "Example tooltip message",
+    trigger: (
+      <Box
+        css={{
+          width: "fit-content",
+        }}
+      >
+        <Text>Some text.</Text>
+      </Box>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+  decorators: [
+    (story) => {
+      return <TooltipProvider>{story()}</TooltipProvider>;
+    },
+  ],
+} as Meta<TooltipProps>;
+
+export const Primary: StoryObj<TooltipProps> = {};
